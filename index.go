c69ae15a656c1f7014d116455cf343bdaf3e6f17
titrxw <1120309488@qq.com>
2021-01-09 22:53:21 +0800

    byte int

--- index.go
+++ index.go
@@ -1,9 +1,34 @@
-package go_study
+package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
-	fmt.Println(1)
+	var test byte = 'A'
+	fmt.Print(test) //通过ascll码表转换
+	fmt.Print(string(test))
+
+	var test1 byte = 65
+	fmt.Print(test1)
+	fmt.Print(string(test1))
+
+	var num uint32 = 1010000000
+	fmt.Print(string(num))
+
+	//go 汉字占三个字节
+	var name uint32 = '国'
+	fmt.Print(name)
+	fmt.Print(string(name))
+	fmt.Print(unsafe.Sizeof(name))
+
+	//go 汉字占三个字节，这里两个字节就可以表示是因为该字较简单，在unicode编码中国的排序靠前
+	var name1 uint16 = '国'
+	fmt.Print(name1)
+	fmt.Print(string(name1))
+	fmt.Print(unsafe.Sizeof(name1))
+
+	var country string = "hello,中国"
+	fmt.Println(len(country)) //5 + 1 + 6
 }
